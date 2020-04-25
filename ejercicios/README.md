f8c5aeaad8cde4021cf93169feff6bbcf7c299d3
jhonatanglorys <jhonatanglorys@gmail.com>
2020-04-25 15:16:18 -0500

    punto 1

--- ejercicios/README.md
+++ ejercicios/README.md
@@ -7,9 +7,9 @@
 ## Ejercicios ##
 
 1. Consulte el uso de la función ```gettimeofday``` para medir el tiempo de ejecución de un programa
-   * ¿Cómo se utiliza esta función (muestre un ejemplo)? 
-   * ¿Cuál es el intervalo de tiempo más corto que se puede medir con esta función?
-2. Consulte cuantas CPUs hay disponibles en el sistema que usted está utilizando. (Pista use el comando lscpu)
+   * ¿Cómo se utiliza esta función (muestre un ejemplo)? R:/ Esta función se utiliza colocando un timer que comienza a contar a partir de cierta línea de código y finaliza luego de cierta línea de código (ver imágenes punto1_codigo.png y punto1_salida.png)
+   * ¿Cuál es el intervalo de tiempo más corto que se puede medir con esta función? R:/ El intervalo mínimo de tiempo que se puede medir con esta función es de 0 milisegundos al no colocar ninguna instrucción entre las líneas de comienzo y finalización del timer (ver imagen punto1-mintime.png)
+2. Consulte cuantas CPUs hay disponibles en el sistema que usted está utilizando. (Pista use el comando lscpu) R:/ Mi sistema tiene 2 procesadores físicos y 4 lógicos.
 2. Desarrolle un programa que utilice un contador concurrente simple. Mida cuánto tiempo lleva incrementar el contador muchas veces a medida que aumenta el número de hilos (1, 2, 4, 8 y 16). Para tal fin, siga las instrucciones que aparecen en el directorio [preciso](./preciso). Una vez complete los pasos continue con el tercer punto.
 3. Desarrolle una versión del contador aproximado (*sloppy counter*) y mida su desempeño a medida que se varía el número de hilos. En este caso, solo maneje un umbral de S = 1024 variando el número de hilos (1, 2, 4, 8 y 16). Las intrucciones para ello se encuentran en el directorio [aproximado](./aproximado).
 4. Grafique los resultados obtenidos de las pruebas recopiladas (en las tablas) para el **número de hilos .vs. el tiempo (en segundos)**, estas siguen la tendencia de la figura **29.5** del libro (la cual se muestra a continuación):
--- ejercicios/preciso/test_precise_counter.c
+++ ejercicios/preciso/test_precise_counter.c
@@ -8,64 +8,91 @@
 /* structs */
 // code here (if you required it)...
 
+
 /* start_routine header */
 // code here...
+void* sumar ();
+int valorMaximo =0;
+int numHilos = 0;
 
 /* Global variables */
 // code here (if you required it)...
+counter_t *contador;
+
 
 int main(int argc, char *argv[]) { 
 
+    if(argc>1){    
+
     /* get the command's line parameters */
     // code here...
+    valorMaximo = atoi(argv[1]);
+    numHilos = atoi(argv[2]);
+    
+    //printf("Contador = %d e hilos = %d\n", valorMaximo, numHilos);
 
 
     /* Declaration of struct timeval variables */
     // code here...
-
+    struct timeval ti, tf;
+    double tiempo;
+    gettimeofday(&ti, NULL);   // Instante inicial
+    
+    
 
     /* Initializing conter */
     // code here...
+    init(&contador);
+    //increment(&contador);
+    //printf("Valor inicial contador %d\n", &contador->value);
 
 
     /* Threads handlers */
     // code here...
+    int i;
+    pthread_t threads_id[argc];
+
+    for(i=1; i<argc;i++){
+        pthread_create(&threads_id[i],NULL, &sumar, NULL);
+    }
 
 
-    /* Thread creation */
-    // code here...
+  
+    for(i=1; i<argc;i++){
+        pthread_join(threads_id[i],NULL);
+    }
 
-
-    /* Time starts counting */
-    // code here...
-
-
-    /* Creating a Threads */
-    // code here...
-   
-
-    /* Threads joins */
-    // code here...
 
 
     /* Time stops counting here */
     // code here...
-
+    gettimeofday(&tf, NULL);   // Instante final
+    
 
     /* get the end time */
     // code here...
+    tiempo= (tf.tv_sec - ti.tv_sec)*1000 + (tf.tv_usec - ti.tv_usec)/1000.0;
     
 
     /* get the elapset time (end_time - start_time) */
     // code here...
-
+    //printf("Has tardado: %g milisegundos\n", tiempo);
 
     /* print the results (number threads employed, counter value, elasep time) */
     // code here...
+    printf("Se han empleado %d hilos para aumentar hasta %d el contador y tomó %g milisegundos para completarlo\n", numHilos, &contador->value, tiempo);
 
-
+    }
     return 0;
 }
 
+
 /* start_routine definition */
 // code here...
+
+void* sumar(){
+    int i;
+    for(i=0; i<valorMaximo;i++){
+        increment(&contador);
+    }
+}
