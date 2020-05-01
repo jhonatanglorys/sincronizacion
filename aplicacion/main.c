9a42f62ca0ba2648ca8e039dac28f77d8f4270c4
jhonatanglorys <jhonatanglorys@gmail.com>
2020-05-01 18:52:03 -0500

    Creacion de libros iniciales

--- aplicacion/main.c
+++ aplicacion/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <string.h>
 #include <stdlib.h>
 #include "book.h"
 
@@ -12,9 +13,13 @@ book libros[10000];
 int main(){
     long i;
     for(i=0; i<10000;i++){
-        setId((libros+i),i); 
+        setYear((libros+i),2020);
+        setPage((libros+i),100);
+        setId((libros+i),i);
+        strcpy(libros[i].name, "Book_");
     }
-    printf("El primer ID es %ld y el ultimo id es %ld\n",getId(&libros[0]), getId(&libros[9999]));
+    printf("El primer ID es %ld, se llama %c,  tiene %d paginas y fue publicado en el año %d\n",getId(&libros[0]),getName(&libros[0]), getPages(&libros[0]), getYear(&libros[0]));
+    printf("El ultimo ID es %ld, se llama %c, tiene %d paginas y fue publicado en el año %d\n",getId(&libros[9999]), getName(&libros[9999]),getPages(&libros[9999]), getYear(&libros[9999]));
     return 0;
 }
 
